62af8884c20d03fdd452d25d32f5231919128e1c
Mirian97 <mirianquispe1505@gmail.com>
2025-09-13 09:38:59 -0300

    feat: initialize NestJS application with basic structure
    - Created AppController to handle root GET request.
    - Implemented AppService to return a greeting message.
    - Set up AppModule with necessary imports and providers.
    - Added main entry point for the application.
    - Included end-to-end tests for the AppController.
    - Configured Jest for end-to-end testing.
    - Added TypeScript configuration files for building and development.

---
+++ server/src/app.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { RolesGuard } from './roles/roles.guard';
+import { UsersModule } from './users/users.module';
+import { CarsModule } from './cars/cars.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(process.env.MONGODB_URI || ''),
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    UsersModule,
+    AuthModule,
+    CarsModule,
+  ],
+  controllers: [AppController],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
+    AppService,
+  ],
+})
+export class AppModule {}
