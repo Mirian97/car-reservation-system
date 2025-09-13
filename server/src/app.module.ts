358eb7d2b0c7c48ac7160cb2c23a7cd347c4f3e4
Mirian97 <mirianquispe1505@gmail.com>
2025-09-13 14:15:35 -0300

    fix: define env global first

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,18 +5,18 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { RolesGuard } from './roles/roles.guard';
-import { UsersModule } from './users/users.module';
 import { CarsModule } from './cars/cars.module';
 import { ReservationsModule } from './reservations/reservations.module';
+import { RolesGuard } from './roles/roles.guard';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.MONGODB_URI || ''),
     ConfigModule.forRoot({
       envFilePath: '.env',
       isGlobal: true,
     }),
+    MongooseModule.forRoot(process.env.MONGODB_URI || ''),
     UsersModule,
     AuthModule,
     CarsModule,
