e97dc25f74a039dfa5f61043789def0062d232e9
Mirian97 <mirianquispe1505@gmail.com>
2025-09-13 09:44:03 -0300

    feat: add Car entity and CarType enum for car management

---
+++ server/src/cars/entities/car.entity.ts
@@ -0,0 +1,30 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { CarType } from '../enums/car-type.enum';
+
+@Schema({ timestamps: true })
+export class Car {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true, min: 1900, max: new Date().getFullYear() })
+  year: number;
+
+  @Prop({
+    required: true,
+    type: String,
+    enum: CarType,
+  })
+  type: CarType;
+
+  @Prop({
+    required: true,
+    min: 0.0,
+    get: (val: number) => Number(val.toFixed(1)),
+  })
+  engine: number;
+
+  @Prop({ required: true, min: 1, max: 20 })
+  size: number;
+}
+
+export const CarSchema = SchemaFactory.createForClass(Car);
---
+++ server/src/cars/enums/car-type.enum.ts
@@ -0,0 +1,14 @@
+export enum CarType {
+  UTILITARIO = 'Utilitário',
+  UTILITARIO_LEVE = 'Utilitário leve',
+  MINIVAN = 'Minivan',
+  PICAPE_MEDIA = 'Picape média',
+  SEDAN_COMPACTO = 'Sedan Compacto',
+  SEDAN_MEDIO = 'Sedan médio',
+  SEDAN_GRANDE = 'Sedan grande',
+  PICAPE_LEVE_MEDIA = 'Picape leve-média',
+  PICAPE_LEVE = 'Picape leve',
+  COUPE = 'Coupé',
+  CROSSOVER = 'Crossover',
+  SUV_GRANDE = 'SUV Grande',
+}
