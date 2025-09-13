0f5dbe70ecb6f6fb9a1cd404e4970a413f42adc4
Mirian97 <mirianquispe1505@gmail.com>
2025-09-13 09:42:33 -0300

    feat: implement users controller and service for user management

---
+++ server/src/users/users.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { UserNotFoundException } from './exception/user-not-found.exception';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const user = await this.userModel.create(createUserDto);
+    return user.save();
+  }
+
+  async findAll() {
+    return await this.userModel.find();
+  }
+
+  async findOne(id: string, withPassword: boolean = false) {
+    const user = await this.userModel
+      .findById(id)
+      .select(withPassword ? '+password' : '-password');
+    if (!user) {
+      throw new UserNotFoundException();
+    }
+    return user;
+  }
+
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    return await this.userModel.findByIdAndUpdate(id, updateUserDto, {
+      new: true,
+    });
+  }
+
+  async remove(id: string) {
+    await this.findOne(id);
+    return await this.userModel.findByIdAndDelete(id);
+  }
+}
