dc5378fb6d9970a5190097198ac755af7bd1ed19
Mirian97 <mirianquispe1505@gmail.com>
2025-09-16 16:36:12 -0300

    feat: add sign-up and home pages
    - Added sign-up component
    - Added home component
    - Updated app routes

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'sign-up', component: SignUpComponent },
+  { path: 'home', component: HomeComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
