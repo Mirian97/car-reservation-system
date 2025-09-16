238274dbe9f533c46541b54b054723ad09b71d7d
Mirian97 <mirianquispe1505@gmail.com>
2025-09-16 18:35:17 -0300

    feat(ui): enhance button and input components
    - Improve button styling
    - Implement form control for input

--- web/src/app/components/input/input.component.ts
+++ web/src/app/components/input/input.component.ts
@@ -1,17 +1,51 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, forwardRef, Input } from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './input.component.html',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true,
+    },
+  ],
 })
 export class InputComponent {
   @Input() type?: 'text' | 'password' | 'email' = 'text';
   @Input() placeholder?: string = '';
-  @Input() value?: string = '';
   @Input() disabled?: boolean = false;
   @Input() className?: string = '';
-  @Input() onChange?: (event: Event) => void = () => {};
+  @Input() showError?: boolean = false;
+  @Input() name?: string = '';
+  value: string = '';
+
+  private onChange: (value: string) => void = () => {};
+  onTouched: () => void = () => {};
+
+  writeValue(value: string): void {
+    this.value = value || '';
+  }
+
+  registerOnChange(fn: typeof this.onChange): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: typeof this.onTouched): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  onInput(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    this.value = input.value;
+    this.onChange(this.value);
+  }
 }
