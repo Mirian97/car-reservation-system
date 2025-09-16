a67425c3c5c38608616a7781e069e2f35c81bd61
Mirian97 <mirianquispe1505@gmail.com>
2025-09-16 11:23:56 -0300

    feat(login): improve login page UI
    - Added logo as background
    - Improved page structure
    - Fixed editorconfig settings

--- web/src/app/pages/login/login.component.ts
+++ web/src/app/pages/login/login.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { ButtonComponent } from '../../components/button/button.component';
 import { InputComponent } from '../../components/input/input.component';
 import { LogoComponent } from '../../components/logo/logo.component';
+import { SvgIconComponent } from '../../components/svg-icon/svg-icon.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ButtonComponent, InputComponent, LogoComponent],
+  imports: [ButtonComponent, InputComponent, LogoComponent, SvgIconComponent],
   templateUrl: './login.component.html',
 })
 export class LoginComponent {}
