e5233d33e3b0cbfaf4850a5235edd920b76a983d
Mirian97 <mirianquispe1505@gmail.com>
2025-09-16 10:33:04 -0300

    feat: Translate app name to Portuguese
    - Updated title in index.html
    - Updated manifest name
    - Updated app component title

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  title = 'Guardians | Car Reservation';
+  title = 'Guardians | Reserva de Carro';
 }
