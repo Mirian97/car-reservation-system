039cfd0dba50c4271505f560f6241a90b3d35543
Mirian97 <mirianquispe1505@gmail.com>
2025-09-17 17:32:00 -0300

    feat: integrate i18n for internationalization
    - Added i18n and nestjs-i18n packages
    - Configured i18n module
    - Set up language resolvers

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,17 +2,30 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AcceptLanguageResolver, I18nModule, QueryResolver } from 'nestjs-i18n';
+import path from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthGuard } from './auth/auth.guard';
 import { AuthModule } from './auth/auth.module';
 import { CarsModule } from './cars/cars.module';
 import { ReservationsModule } from './reservations/reservations.module';
 import { RolesGuard } from './roles/roles.guard';
 import { UsersModule } from './users/users.module';
-import { AuthGuard } from './auth/auth.guard';
 
 @Module({
   imports: [
+    I18nModule.forRoot({
+      fallbackLanguage: 'pt',
+      loaderOptions: {
+        path: path.join(__dirname, '/i18n/'),
+        watch: true,
+      },
+      resolvers: [
+        { use: QueryResolver, options: ['lang'] },
+        AcceptLanguageResolver,
+      ],
+    }),
     ConfigModule.forRoot({
       envFilePath: '.env',
       isGlobal: true,
