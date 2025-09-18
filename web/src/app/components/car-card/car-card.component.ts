91324e6266658db904df2b15e27f0ee7c6ecee0c
Mirian97 <mirianquispe1505@gmail.com>
2025-09-18 16:20:27 -0300

    feat(car): display car details on card
    - Added car type
    - Added pluralize pipe
    - Added default car image

--- web/src/app/components/car-card/car-card.component.ts
+++ web/src/app/components/car-card/car-card.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { PluralizePipe } from '@/app/common/pipes/pluralize.pipe';
+import { Car } from '@/app/types/car.type';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-car-card',
   standalone: true,
-  imports: [],
+  imports: [PluralizePipe],
   templateUrl: './car-card.component.html',
 })
-export class CarCardComponent {}
+export class CarCardComponent {
+  @Input() car!: Car;
+}
