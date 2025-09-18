8f60cae0bbb636f17e93beca6920d732f6c1d569
Mirian97 <mirianquispe1505@gmail.com>
2025-09-18 12:12:22 -0300

    feat: add profile and reservation pages
    - Created profile component
    - Created reservation component
    - Added error messages constant

--- web/src/app/pages/home/home.component.ts
+++ web/src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
+import { NavbarComponent } from '@/app/components/navbar/navbar.component';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {}
