cf6bccb576e76210420a0763ae594d5bcfb46edc
Mirian97 <mirianquispe1505@gmail.com>
2025-09-18 13:03:33 -0300

    feat(navbar): improve navbar appearance and behavior
    - Added active link styling
    - Improved hover effect
    - Added background color to auth layout

--- web/src/app/components/navbar/navbar.component.ts
+++ web/src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,12 @@
 import { navbar } from '@/app/constants/navbar.constant';
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { SvgIconComponent } from '../svg-icon/svg-icon.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, SvgIconComponent],
+  imports: [RouterModule, SvgIconComponent],
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {
