b2e6d6a0748bd7547303373713b15f8a6bfde291
Mirian97 <mirianquispe1505@gmail.com>
2025-09-17 21:45:55 -0300

    feat: use I18nValidationPipe
    - Replaced ValidationPipe
    - Implemented I18n validation

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,12 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { I18nValidationPipe } from 'nestjs-i18n';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(
-    new ValidationPipe({
+    new I18nValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
       transform: true,
