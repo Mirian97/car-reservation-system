98d28638f62b740bcd44c59fa47c1f0cd6ee3313
Mirian97 <mirianquispe1505@gmail.com>
2025-09-18 15:58:00 -0300

    feat: create car card component
    - Created car card component
    - Added basic HTML structure

---
+++ web/src/app/components/car-card/car-card.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-car-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './car-card.component.html',
+})
+export class CarCardComponent {}
