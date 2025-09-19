7e7ce65f0abcc0b6e147aeb586123ddce9442f73
Mirian97 <mirianquispe1505@gmail.com>
2025-09-19 12:35:38 -0300

    feat(filter): add initial filter page layout
    - Added search icon
    - Added back to search link
    - Added apply filter button

--- web/src/app/pages/filter/filter.component.ts
+++ web/src/app/pages/filter/filter.component.ts
@@ -1,9 +1,12 @@
+import { ButtonComponent } from '@/app/components/button/button.component';
+import { SvgIconComponent } from '@/app/components/svg-icon/svg-icon.component';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-filter',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent, SvgIconComponent, RouterLink],
   templateUrl: './filter.component.html',
 })
 export class FilterComponent {}
