9e387291e685fee6efbc612e95bd361b8f2ae05c
Mirian97 <mirianquispe1505@gmail.com>
2025-09-20 12:12:38 -0300

    feat(home): add debounced input for search
    - Replaced input with debounced input
    - Added onSearch function
    - Navigates to search query params

--- web/src/app/pages/home/home.component.ts
+++ web/src/app/pages/home/home.component.ts
@@ -1,20 +1,29 @@
 import { AuthService } from '@/app/auth/auth.service';
+import { DebouncedInputComponent } from '@/app/components/debounced-input/debounced-input.component';
 import { ListCarsComponent } from '@/app/components/list-cars/list-cars.component';
 import { SvgIconComponent } from '@/app/components/svg-icon/svg-icon.component';
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, SvgIconComponent, ListCarsComponent, RouterLink],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    SvgIconComponent,
+    ListCarsComponent,
+    RouterLink,
+    DebouncedInputComponent,
+  ],
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit {
   greeting!: string;
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
     this.getGreeting();
@@ -23,5 +32,12 @@ export class HomeComponent implements OnInit {
   getGreeting(): void {
     const userName = this.authService.getUser()?.name;
     this.greeting = userName ? `Olá, ${userName}` : 'Olá!';
+  }
+
+  onSearch(value: string) {
+    this.router.navigate([], {
+      queryParams: { name: value || null },
+      queryParamsHandling: 'merge',
+    });
   }
 }
