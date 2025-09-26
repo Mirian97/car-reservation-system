ad552e17abcf9c105eb162baff00705d4ca43457
Mirian97 <mirianquispe1505@gmail.com>
2025-09-25 21:18:03 -0300

    feat(car): add car creation form submission
    - Handle form submission
    - Add takeUntilDestroyed pipe

--- web/src/app/components/create-car-drawer/create-car-drawer.component.ts
+++ web/src/app/components/create-car-drawer/create-car-drawer.component.ts
@@ -7,7 +7,15 @@ import { toast } from '@/app/helpers/toast';
 import { CarService } from '@/app/services/car.service';
 import { CarType } from '@/app/types/car.type';
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  DestroyRef,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import {
   FormBuilder,
   FormGroup,
@@ -49,6 +57,7 @@ export class CreateCarDrawerComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private carService: CarService,
+    private destroyRef: DestroyRef,
   ) {}
 
   onClose() {
@@ -92,6 +101,7 @@ export class CreateCarDrawerComponent implements OnInit {
     const formValues = this.createCarForm.value;
     this.carService
       .create(formValues)
+      .pipe(takeUntilDestroyed(this.destroyRef))
       .subscribe({
         next: () => {
           toast.success({ text: 'Carro cadastrado!' });
