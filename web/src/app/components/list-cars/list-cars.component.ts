cf2ce2d5020cc6515b50c97702ccc4cab10e6413
Mirian97 <mirianquispe1505@gmail.com>
2025-09-28 18:20:28 -0300

    refactor(list-cars): unsubscribe from queryParams
    - Use takeUntilDestroyed to unsubscribe
    - Prevents memory leaks

--- web/src/app/components/list-cars/list-cars.component.ts
+++ web/src/app/components/list-cars/list-cars.component.ts
@@ -2,7 +2,8 @@ import { AuthService } from '@/app/auth/auth.service';
 import { CarService } from '@/app/services/car.service';
 import { Car, SearchCarsFilters } from '@/app/types/car.type';
 import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ActivatedRoute, Params } from '@angular/router';
 import { BookingCarDrawerComponent } from '../booking-car-drawer/booking-car-drawer.component';
 import { CarCardComponent } from '../car-card/car-card.component';
@@ -23,6 +24,8 @@ export class ListCarsComponent implements OnInit {
   authService = inject(AuthService);
   carService = inject(CarService);
   route = inject(ActivatedRoute);
+  destroyRef = inject(DestroyRef);
+
   drawerOpen = false;
   selectedCar?: Car;
 
@@ -31,15 +34,17 @@ export class ListCarsComponent implements OnInit {
   }
 
   searchCars(): void {
-    this.route.queryParams.subscribe((params: Params) => {
-      const filters: Partial<SearchCarsFilters> = {
-        name: params['name'] || '',
-        type: params['type'] || [],
-        engine: params['engine'] || [],
-        size: params['size'] || [],
-      };
-      this.carService.searchCars(filters);
-    });
+    this.route.queryParams
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((params: Params) => {
+        const filters: Partial<SearchCarsFilters> = {
+          name: params['name'] || '',
+          type: params['type'] || [],
+          engine: params['engine'] || [],
+          size: params['size'] || [],
+        };
+        this.carService.searchCars(filters);
+      });
   }
 
   openDrawer(car: Car) {
