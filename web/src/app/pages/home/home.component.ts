523bd55b16c1f46841198daf1d4edf7e75ba3c70
Mirian97 <mirianquispe1505@gmail.com>
2025-09-28 21:26:41 -0300

    refactor(home): use observable for filter active
    - Use async pipe in template
    - Update queryParams handling

--- web/src/app/pages/home/home.component.ts
+++ web/src/app/pages/home/home.component.ts
@@ -10,6 +10,7 @@ import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -32,33 +33,34 @@ export class HomeComponent implements OnInit {
   route = inject(ActivatedRoute);
   authService = inject(AuthService);
   formBuilder = inject(FormBuilder);
-  searchForm!: FormGroup;
-  hasFilterActive!: boolean;
   drawerOpen = false;
+  searchForm!: FormGroup;
+  hasFilterActive$: Observable<boolean> = this.route.queryParams.pipe(
+    map((params) => objectHasValidProperties(params)),
+  );
 
   ngOnInit(): void {
+    this.searchForm = this.formBuilder.group({
+      name: [''],
+    });
     this.route.queryParams.subscribe((params) => {
-      this.hasFilterActive = objectHasValidProperties(params);
-      const name = params?.['name'] ?? '';
-      this.searchForm = this.formBuilder.group({
-        name,
-      });
+      this.searchForm.patchValue({ name: params?.['name'] ?? '' });
     });
   }
 
   onSearch(value: string) {
+    const queryParams = value ? { name: value } : {};
     this.router.navigate([], {
-      queryParams: { name: value || null },
+      queryParams,
       queryParamsHandling: 'merge',
     });
   }
 
   onNavigateFilters(): void {
-    this.route.queryParams.subscribe((params) => {
-      this.router.navigate(['/inicio/filtrar'], {
-        queryParams: params,
-        queryParamsHandling: 'merge',
-      });
+    const params = this.route.snapshot.queryParams;
+    this.router.navigate(['/inicio/filtrar'], {
+      queryParams: params,
+      queryParamsHandling: 'merge',
     });
   }
 
